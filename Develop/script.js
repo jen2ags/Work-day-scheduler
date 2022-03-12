0234db18239311746b96135d0f929eabaca5d447
jennj <jennjenn93@gmail.com>
2022-03-11 19:18:22 -0600

    console.log current day

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+var currentDay = document.getElementById('#currentDay');
+
+//display the date and time on the page
+var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(currentDay);
