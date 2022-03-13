ed43e6c93d0da6e18f809898fb0b5e231895ddbd
jennj <jennjenn93@gmail.com>
2022-03-12 18:55:56 -0600

    color change depending on time

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,31 @@
 var currentDay = document.getElementById('currentDay');
 var currentDayDisplay = moment().format('MMMM Do YYYY, h:mm:ss a');
-var hourDisplay = getElementById('hour');
+var hourDisplay = $('.hour');
 var saveButtonEl =$('.saveBtn');
+var hours = moment().hours();
+
+
 
 //display the date and time on the page
 var currentDayEl = document.createElement('p');
     currentDayEl.textContent = currentDayDisplay; 
     currentDay.append(currentDayDisplay);
 
+function timeBlock() {
+    
+    if (hourDisplay === hours) {
+        console.log('present');
+    }
+    else if (hourDisplay < hours) {
+        console.log('past');
+    }
+    else  { 
+        console.log('future');
+    }
+}
+    
+
 
 saveButtonEl.on('click', function() {
-    
+
 })    
